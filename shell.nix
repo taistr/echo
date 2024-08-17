7e05947355d414cdd13ea4bed91d6e4220319cf9
Tyson Truong <tystruong@gmail.com>
2024-08-18 00:31:28 +1000

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,9 @@
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell {
+    # nativeBuildInputs is usually what you want -- tools you need to run
+    nativeBuildInputs = with pkgs.buildPackages; [ 
+      python3
+      python312Packages.jupyter
+      python312Packages.langchain
+     ];
+}
