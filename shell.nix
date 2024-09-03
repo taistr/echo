d3ae20b41d0547f72b56704a3a5d1bc1eee258bb
Tyson Truong <tystruong@gmail.com>
2024-09-03 14:00:17 +1000

    Implementing a CLI MVP

--- shell.nix
+++ shell.nix
@@ -1,17 +1,20 @@
-{ pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
-    # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = with pkgs.buildPackages; [ 
-      python3
-      jupyter
-      openai
-     ];
+{
+  pkgs ? import <nixpkgs> { },
+}:
+pkgs.mkShell {
+  # nativeBuildInputs is usually what you want -- tools you need to run
+  nativeBuildInputs = with pkgs.buildPackages; [
+    python3
+    python3Packages.jupyter
+    python3Packages.openai
+    python3Packages.black
+    python3Packages.pyzmq
+    python3Packages.prompt-toolkit
+  ];
 
-  shellHook =
-  ''
+  shellHook = ''
     echo "~~ECHO SHELL~~"
     export OPENAI_API_KEY="$(cat ~/.config/echo/echo_openai_key)"
   '';
 
-     
 }
